2c4bedf98e709e290502aa973f06b938a9921866
Sonny Spaan <info@spaanproductions.nl>
2021-06-17 00:01:18 +0200

    Update PHP-CS-Fixer settings

--- .php_cs.dist.php
+++ .php_cs.dist.php
@@ -12,29 +12,41 @@ $finder = Symfony\Component\Finder\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setRules([
-        '@PSR2' => true,
-        'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
-        'no_unused_imports' => true,
-        'not_operator_with_successor_space' => true,
-        'trailing_comma_in_multiline' => true,
-        'phpdoc_scalar' => true,
-        'unary_operator_spaces' => true,
-        'binary_operator_spaces' => true,
-        'blank_line_before_statement' => [
+        '@PSR2'                              => true,
+        'array_syntax'                       => ['syntax' => 'short'],
+        'ordered_imports'                    => ['sort_algorithm' => 'length'],
+        'no_unused_imports'                  => true,
+        'not_operator_with_successor_space'  => true,
+        'trailing_comma_in_multiline'        => true,
+        'phpdoc_scalar'                      => true,
+        'unary_operator_spaces'              => true,
+        'binary_operator_spaces'             => true,
+        'blank_line_before_statement'        => [
             'statements' => ['break', 'continue', 'declare', 'return', 'throw', 'try'],
         ],
-        'phpdoc_single_line_var_spacing' => true,
-        'phpdoc_var_without_name' => true,
-        'class_attributes_separation' => [
+        'phpdoc_single_line_var_spacing'     => true,
+        'phpdoc_var_without_name'            => true,
+        'class_attributes_separation'        => [
             'elements' => [
                 'method' => 'one',
             ],
         ],
-        'method_argument_space' => [
-            'on_multiline' => 'ensure_fully_multiline',
+        'method_argument_space'              => [
+            'on_multiline'                     => 'ensure_fully_multiline',
             'keep_multiple_spaces_after_comma' => true,
         ],
-        'single_trait_insert_per_statement' => true,
+        'single_trait_insert_per_statement'  => true,
+        'php_unit_method_casing'             => [
+            'case' => 'snake_case'
+        ],
+        'linebreak_after_opening_tag'        => true,
+        'blank_line_after_opening_tag'       => true,
+        'combine_consecutive_issets'         => true,
+        'combine_consecutive_unsets'         => true,
+        'no_short_bool_cast'                 => true,
+        'no_short_echo_tag'                  => true,
+        'not_operator_with_space'            => true,
+        'object_operator_without_whitespace' => true,
     ])
+    ->setIndent("\t")
     ->setFinder($finder);
