c03e32a3fb43cb5f00eab9946bd0ba2e22518a57
spaantje <spaantje@users.noreply.github.com>
2021-06-19 18:51:19 +0000

    Fix styling

--- src/Commands/ManageLaravelStatsCommand.php
+++ src/Commands/ManageLaravelStatsCommand.php
@@ -17,61 +17,61 @@ use Spaanproductions\ManageLaravelStats\ShareableMetrics\Metrics\ManageLaravelTe
 
 class ManageLaravelStatsCommand extends Command
 {
-    public $signature = 'manage-laravel-stats {--dry-run}';
+	public $signature = 'manage-laravel-stats {--dry-run}';
 
-    public $description = 'My command';
+	public $description = 'My command';
 
-    public function handle()
-    {
-        $data = collect([
-            ManageLaravelTeam::class,
-            Name::class,
-            Url::class,
-            GitInfo::class,
-            InstalledPackages::class,
-            PhpVersion::class,
-            LaravelVersion::class,
-            ServerInfo::class,
-            ScheduledTasks::class,
-        ])->map(function (string $metricClass) {
-            return new $metricClass();
-        })->map(function (Metric $metric) {
-            return $metric->toArray();
-        })->mapWithKeys(function ($item) {
-            return [
-                key($item) => $item[key($item)],
-            ];
-        });
+	public function handle()
+	{
+		$data = collect([
+			ManageLaravelTeam::class,
+			Name::class,
+			Url::class,
+			GitInfo::class,
+			InstalledPackages::class,
+			PhpVersion::class,
+			LaravelVersion::class,
+			ServerInfo::class,
+			ScheduledTasks::class,
+		])->map(function (string $metricClass) {
+			return new $metricClass();
+		})->map(function (Metric $metric) {
+			return $metric->toArray();
+		})->mapWithKeys(function ($item) {
+			return [
+				key($item) => $item[key($item)],
+			];
+		});
 
-        if ($this->option('dry-run')) {
-            dump($data->toArray());
+		if ($this->option('dry-run')) {
+			dump($data->toArray());
 
-            return 0;
-        }
+			return 0;
+		}
 
-        $url = app()->isLocal()
-            ? 'http://managelaravel.test/api/stats'
-            : 'https://manage-laravel.spaan.dev/api/stats';
+		$url = app()->isLocal()
+			? 'http://managelaravel.test/api/stats'
+			: 'https://manage-laravel.spaan.dev/api/stats';
 
-        $response = Http::withHeaders([
-            'Accept'        => 'application/json',
-            'Authorization' => 'Bearer ' . config('manage-stats.token'),
-        ])->post($url, $data->toArray());
+		$response = Http::withHeaders([
+			'Accept' => 'application/json',
+			'Authorization' => 'Bearer ' . config('manage-stats.token'),
+		])->post($url, $data->toArray());
 
-        dump($response->body());
+		dump($response->body());
 
-        if ( ! $response->ok()) {
-            $this->error('Something went wrong..');
-            $this->error($response->json('message'));
+		if ( ! $response->ok()) {
+			$this->error('Something went wrong..');
+			$this->error($response->json('message'));
 
-            return 1;
-        }
+			return 1;
+		}
 
 
-        $this->comment('All done');
+		$this->comment('All done');
 
-        $this->info(sprintf('The current state is "%s", latest Laravel version is %s', $response->json('status'), $response->json('desired_laravel_version')));
+		$this->info(sprintf('The current state is "%s", latest Laravel version is %s', $response->json('status'), $response->json('desired_laravel_version')));
 
-        return 0;
-    }
+		return 0;
+	}
 }
